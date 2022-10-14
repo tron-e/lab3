7576582d2d350185a72904ef0f8c88fa237c78f8
tron-e <93411591+tron-e@users.noreply.github.com>
2022-10-14 14:34:16 -0700

    test cases

--- ListTests.java
+++ ListTests.java
@@ -5,6 +5,13 @@ public class ListTests{
 
     @Test
     public void test1(){
+        String[] input1 = {"apple", "cat", "effort"};
+        String[] input2 = {"bat", "dap", "fail"};
+        String[] ans = {"apple", "bat","cat","dap", "effort", "fail"};
+
+        List<String> list1 = makeList(input1);
         
+
+        assertArrayEquals(ans, ListExamples.merge);
     }
 }
